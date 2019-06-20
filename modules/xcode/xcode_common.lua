2b9390ef877ba47813e0559433d8fefb142a5d4c
andryblack <blackicebox@gmail.com>
2019-06-20 19:59:40 +0300

    xcconfig files support

--- modules/xcode/xcode_common.lua
+++ modules/xcode/xcode_common.lua
@@ -1242,6 +1242,7 @@
 
 		_p(2,'%s /* %s */ = {', cfg.xcode.targetid, cfg.buildcfg)
 		_p(3,'isa = XCBuildConfiguration;')
+		xcodePrintUserConfigReferences(3,cfg,tr,'target')
 		_p(3,'buildSettings = {')
 		printSettingsTable(4, settings)
 		_p(3,'};')
@@ -1467,6 +1468,7 @@
 
 		_p(2,'%s /* %s */ = {', cfg.xcode.projectid, cfg.buildcfg)
 		_p(3,'isa = XCBuildConfiguration;')
+		xcodePrintUserConfigReferences(3,cfg,tr,'project')
 		_p(3,'buildSettings = {')
 		printSettingsTable(4, settings)
 		_p(3,'};')
